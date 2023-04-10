ee015a6829c78764baa3592919c945d35a4e92fc
EduardoWagner03 <eduardogwagner2003@gmail.com>
2023-04-10 20:55:45 -0300

    add dashbord

--- js/script.js
+++ js/script.js
@@ -9,3 +9,23 @@ const btn_sair = document.querySelector("#sair");
     }
       })
 
+// Selecionar os itens clicado
+var itemMenu = document.querySelectorAll('.item-menu')
+function selectlink(){
+    itemMenu.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+itemMenu.forEach((item)=>
+    item.addEventListener('click',selectlink)
+)
+//expandir o menu
+
+var btnExp = document.querySelector('#btn-exp')
+var menuLateral = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click',function(){
+    menuLateral.classList.toggle('expandir')
+})   
