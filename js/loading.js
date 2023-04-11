b907411425172176ca3e3eafbb1383112e09bed6
Victor Bueno <victorbueno2003@hotmail.com>
2023-04-11 10:31:58 -0300

    adicionado componente de carregamento

---
+++ js/loading.js
@@ -0,0 +1,18 @@
+function showLoading() {
+    const div = document.createElement("div");
+    div.classList.add("loading");
+    
+    const span = document.createElement("span");
+    span.classList.add("carregando");
+    
+    div.appendChild(span);
+    document.body.insertBefore(div, document.body.firstChild);
+    
+  }
+  
+function hideloading(){
+    const loadings = document.getElementsByClassName("loading");
+    if(loadings.length){
+        loadings[0].remove()
+    }
+}
