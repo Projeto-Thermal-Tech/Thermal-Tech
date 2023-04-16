15922e55821258563417f5db6c1ce31a559346b2
Victor Bueno <victorbueno2003@hotmail.com>
2023-04-16 17:27:01 -0300

    função de criar conta e esquecer senha

--- public/Login_v1/script_login.js
+++ public/Login_v1/script_login.js
@@ -1,61 +1,119 @@
 // Add this code to the login page JavaScript file
-window.onload = function() {
-    document.getElementById("login-form").elements[0].focus();
-  };
-  
+window.onload = function () {
+  document.getElementById("login-form").elements[0].focus();
+};
+function signIn() {
+  showLoading();
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password).then((response) => {
+      hideloading();
+      window.location.href = "../pages/home.html";
+    }).catch((error) => {
+      hideloading();
+      alert("Usuario não encontrado");
+    });
+}
+function newCont() {
+  window.location.href = "./criar.html";
+}
+
+function register() {
+    showLoading();
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    firebase.auth().createUserWithEmailAndPassword(email,password).then(()=>{
+        hideloading()
+        window.location.href ='../pages/home.html'
+    }).catch(error =>{
+        hideloading()
+        alert(error)
+    })
+  }
+
+
+
+
+function recoverPassword(){
+    let email = document.getElementById("email").value;
+    showLoading()
+    firebase.auth().sendPasswordResetEmail(email).then(()=>{
+        hideloading()
+        alert("email enviado com sucesso")
+    }).catch(error =>{
+        hideloading();
+        alert("email não encontrado")
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 (function ($) {
-    "use strict";
+  "use strict";
 
-    
-    /*==================================================================
+  /*==================================================================
     [ Validate ]*/
-    var input = $('.validate-input .input100');
+  var input = $(".validate-input .input100");
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+  $(".validate-form").on("submit", function () {
+    var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
+    for (var i = 0; i < input.length; i++) {
+      if (validate(input[i]) == false) {
+        showValidate(input[i]);
+        check = false;
+      }
+    }
 
-        return check;
+    return check;
+  });
+
+  $(".validate-form .input100").each(function () {
+    $(this).focus(function () {
+      hideValidate(this);
     });
+  });
 
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
+  function validate(input) {
+    if ($(input).attr("type") == "email" || $(input).attr("name") == "email") {
+      if (
+        $(input)
+          .val()
+          .trim()
+          .match(
+            /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+          ) == null
+      ) {
+        return false;
+      }
+    } else {
+      if ($(input).val().trim() == "") {
+        return false;
+      }
     }
+  }
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
+  function showValidate(input) {
+    var thisAlert = $(input).parent();
 
-        $(thisAlert).addClass('alert-validate');
-    }
+    $(thisAlert).addClass("alert-validate");
+  }
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
+  function hideValidate(input) {
+    var thisAlert = $(input).parent();
 
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
-
+    $(thisAlert).removeClass("alert-validate");
+  }
 })(jQuery);
