a6a6aee3384767b66414d02bbd8fe9d85bb41d93
Victor Bueno <victorbueno2003@hotmail.com>
2023-08-06 13:40:40 -0300

    Tela de chamado criada

--- public/js/script.js
+++ public/js/script.js
@@ -53,5 +53,60 @@ function deletar(){
   confirm("Tem certeza que deseja excluir o chamado?")
 
 
+  // Função para gerar o PDF
+  function generatePDF() {
+    // Capturar a área específica da página que você deseja incluir no PDF.
+    // Neste exemplo, iremos capturar todo o corpo da página, mas você pode ajustar
+    // o seletor CSS para capturar apenas a área desejada.
+    const element = document.body;
+
+    // Opções de configuração do html2canvas
+    const options = {
+      scale: 2, // Aumentar a escala para melhor qualidade de imagem (opcional)
+      useCORS: true // Permitir o uso de recursos externos (opcional)
+    };
+
+    // Capturar a imagem da página usando o html2canvas
+    html2canvas(element, options).then(canvas => {
+      // Obter os dados da imagem do canvas
+      const imgData = canvas.toDataURL("image/png");
+
+      // Configurar o tamanho do PDF para corresponder à imagem capturada
+      const pdf = new jsPDF({
+        orientation: "portrait",
+        unit: "px",
+        format: "a4"
+      });
+
+      // Adicionar a imagem ao PDF
+      pdf.addImage(imgData, "PNG", 0, 0);
+
+      // Salvar o PDF (neste exemplo, o nome do arquivo será "pagina.pdf")
+      pdf.save("pagina.pdf");
+    });
+  }
+
+  // Vincular o botão ao evento de clique
+  document.getElementById("btnGeneratePDF").addEventListener("click", generatePDF);
+ }
+function imprimir(){
+  print()
 }
 
+function salvarChamado(){
+  const inputs = document.querySelectorAll(".data-chamado")
+  alert("Chamado criado com sucesso!")
+  inputs.forEach(function(input) {
+    input.value = '';
+  });
+}
+function deletarChamado(){
+  const inputs = document.querySelectorAll(".data-chamado")
+  confirmDelete = confirm("Tem certeza que deseja excluir esse chamado?") 
+  if(confirmDelete = true){
+    inputs.forEach(function(input) {
+      input.value = '';
+    });
+  }
+  
+}
