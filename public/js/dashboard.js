322daf3ddb78655a8c64ff8e546d6dfd18763d0d
EduardoWagner03 <eduardogwagner2003@gmail.com>
2024-04-02 11:18:42 -0300

    Segunda Etapa do dashboard
    Nesta segunda etapa do dashboard, fiz algumas mudanças nos estilos e adicionei novas funcionalidades.
    Agora, os usuários podem adicionar suas próprias fotos de perfil ou a logo da empresa em que trabalham.
    Más essas fotos não estão salvando ou seja quando voce for mudar de tela a imagem vai sumir.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,4 @@
-function toggleDashboard() {
+function AbrirDashboard() {
     const dashboard = document.getElementById("dashboard");
     if (dashboard.style.display === "none") {
         dashboard.style.display = "block";
@@ -12,3 +12,16 @@ function closeDashboard() {
     dashboard.style.display = 'none';
 }
 
+function previewImage(event) {
+    const reader = new FileReader();
+    reader.onload = function() {
+        const output = document.getElementById('preview');
+        output.src = reader.result;
+        output.style.display = 'block';
+  
+        // Esconde o ícone do usuário
+        const icone = document.getElementById('icone');
+        icone.style.display = 'none';
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  }
