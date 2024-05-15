2f064519302f33e4ecde96c67f9a1f768287d6b8
EduardoWagner03 <eduardogwagner2003@gmail.com>
2024-05-14 21:45:30 -0300

    Feito alteração no arquivo notification.py feito por (Luiz Gabriel Roberti)

--- public/python/notification.py
+++ public/python/notification.py
@@ -3,29 +3,44 @@ import os
 import requests
 from win11toast import toast
 
-# Se dois argumentos foram passados para o script, eles serão usados como o ID do chamado e criado_por_cha
-if len(sys.argv) > 2:
-    id_chamado = sys.argv[1]
-    criado_por_cha = ' '.join(sys.argv[2:])
-else:
-    id_chamado = "Nenhum ID fornecido"
-    criado_por_cha = "Nenhum nome fornecido"
+def get_arguments():
+    # Se dois argumentos foram passados para o script, eles serão usados como o ID do chamado e criado_por_cha
+    if len(sys.argv) > 2:
+        id_chamado = sys.argv[1]
+        criado_por_cha = ' '.join(sys.argv[2:])
+    else:
+        id_chamado = "Nenhum ID fornecido"
+        criado_por_cha = "Nenhum nome fornecido"
+    
+    return id_chamado, criado_por_cha
 
-# A mensagem da notificação incluirá o ID do chamado e criado_por_cha
-message = 'Novo Chamado Criado: ' + id_chamado + ', Criado por: ' + criado_por_cha
+def download_image(url, local_path):
+    # Baixe a imagem
+    response = requests.get(url)
 
-# URL da imagem
-url = "https://firebasestorage.googleapis.com/v0/b/thermal-tech-57a87.appspot.com/o/logo2.ico?alt=media&token=2658deae-dbca-4703-ab5b-5f4aaa169cd9"
+    # Salve imagem localmente
+    with open(local_path, 'wb') as file:
+        file.write(response.content)
 
-# Caminho local para salvar a imagem
-local_path = "logo2.ico"
+def create_toast(id_chamado, criado_por_cha, local_path):
+    # A mensagem da notificação incluirá o ID do chamado e criado_por_cha
+    title = f'🔔 Novo Chamado Criado 🔔'
+    message = f'Chamado ID: {id_chamado}\nCriado por: {criado_por_cha}\nClique aqui para visualizar os detalhes.'
 
-# Baixe a imagem
-response = requests.get(url)
+    # Use o caminho local para a imagem como ícone
+    toast(title, message, icon=os.path.realpath(local_path), on_click=f'http://localhost:5000/view/chamado/{id_chamado}')
 
-# Salve imagem localmente
-with open(local_path, 'wb') as file:
-    file.write(response.content)
+def main():
+    id_chamado, criado_por_cha = get_arguments()
 
-# Use o caminho local para a imagem como ícone
-toast(message, icon=os.path.realpath(local_path), on_click=f'http://localhost:5000/view/chamado/{id_chamado}')
+    # URL da imagem
+    url = "https://firebasestorage.googleapis.com/v0/b/thermal-tech-57a87.appspot.com/o/logo2.ico?alt=media&token=2658deae-dbca-4703-ab5b-5f4aaa169cd9"
+
+    # Caminho local para salvar a imagem
+    local_path = "logo2.ico"
+
+    download_image(url, local_path)
+    create_toast(id_chamado, criado_por_cha, local_path)
+
+if __name__ == "__main__":
+    main()
